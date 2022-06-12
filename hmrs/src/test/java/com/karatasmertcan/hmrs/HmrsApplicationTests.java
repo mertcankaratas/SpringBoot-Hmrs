d071aea4f217105fc65ad1867a9fb189a45f7464
Mertcan Karataş <can.mert.karatas@gmail.com>
2022-06-12 16:40:06 +0300

    First Commit

---
+++ hmrs/src/test/java/com/karatasmertcan/hmrs/HmrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karatasmertcan.hmrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
